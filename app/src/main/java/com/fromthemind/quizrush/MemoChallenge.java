0a4f0c9821acd74f0387bec3a26315be641b6e6c
Mehmet Berk Gürçay <mehmetgurcay@ku.edu.tr>
2017-05-17 16:46:41 +0300

    memochallenge firebase connection

--- app/src/main/java/com/fromthemind/quizrush/MemoChallenge.java
+++ app/src/main/java/com/fromthemind/quizrush/MemoChallenge.java
@@ -1,34 +1,47 @@
 package com.fromthemind.quizrush;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * Created by Melih on 17.05.2017.
  */
 
 public class MemoChallenge extends Challenge implements RushListItem<MemoChallenge> {
 
-    public int[] getTargetFlags() {
+    public ArrayList<Integer> getTargetFlags() {
         return targetFlags;
     }
 
-    public void setTargetFlags(int[] targetFlags) {
+    public void setTargetFlags(ArrayList<Integer> targetFlags) {
         this.targetFlags = targetFlags;
     }
 
-    public int[] targetFlags;
+    public ArrayList<Integer> targetFlags = new ArrayList<Integer> ();
 
-    public int[][] getBoardFlags() {
+    public ArrayList<ArrayList<Integer>> getBoardFlags() {
         return boardFlags;
     }
 
-    public void setBoardFlags(int[][] boardFlags) {
+    public void setBoardFlags(ArrayList<ArrayList<Integer>> boardFlags) {
         this.boardFlags = boardFlags;
     }
 
-    public int[][] boardFlags;
+    public ArrayList<ArrayList<Integer>> boardFlags = new ArrayList<ArrayList<Integer>>();
     public MemoChallenge(String challenger,int score_er,String challengee,int score_ee,int[] targetFlags,int[][] boardFlags) {
         super(challenger,score_er,challengee,score_ee);
-        this.targetFlags =targetFlags;
-        this.boardFlags=boardFlags;
+
+        for (int i = 0; i < boardFlags.length; i++) {
+            this.targetFlags.add(targetFlags[i]);
+        }
+        for (int i = 0; i < boardFlags.length; i++) {
+            ArrayList<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j < boardFlags[0].length; j++) {
+                row.add(boardFlags[i][j]);
+            }
+            this.boardFlags.add(row);
+        }
     }
 
     public MemoChallenge() {
